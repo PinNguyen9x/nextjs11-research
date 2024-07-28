108c6e31f0817c7335c3980adb938decb69d73d6
Pinnguyen <nguyenthanhpin95@gamil.com>
2024-07-28 14:40:13 +0700

    Implement footer UI

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,10 +1,42 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Facebook, Instagram, Twitter, LinkedIn } from '@mui/icons-material'
+import { Box, Icon, Stack, Typography } from '@mui/material'
 
 export function Footer() {
+  const socialLinks = [
+    {
+      icon: Facebook,
+      url: 'https://www.facebook.com',
+    },
+    {
+      icon: Twitter,
+      url: 'https://www.twitter.com',
+    },
+    {
+      icon: Instagram,
+      url: 'https://www.instagram.com',
+    },
+    {
+      icon: LinkedIn,
+      url: 'https://www.linkedin.com',
+    },
+  ]
   return (
     <Box component={'footer'} sx={{ py: 3, px: 2, textAlign: 'center' }}>
-      Footer
+      <Stack direction="row" justifyContent="center">
+        {socialLinks.map((link, index) => (
+          <Box
+            component="a"
+            href={link.url}
+            key={index}
+            p={2}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon component={link.icon} sx={{ fontSize: 48 }} />
+          </Box>
+        ))}
+      </Stack>
+      <Typography>Copyright ©{new Date().getFullYear()} All rights reserved </Typography>
     </Box>
   )
 }
