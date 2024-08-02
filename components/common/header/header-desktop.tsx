798f4cafa4585801e52860f28dc2dd474e1bd409
Pinnguyen <nguyenthanhpin95@gamil.com>
2024-08-02 12:47:45 +0700

    Show hide menu base on login status

--- components/common/header/header-desktop.tsx
+++ components/common/header/header-desktop.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import { Box, Container, Stack, Link as MuiLink } from '@mui/material'
-import { ROUTE_LIST } from './routes'
-import Link from 'next/link'
+import { useAuth } from '@/hooks'
+import { Box, Container, Link as MuiLink, Stack } from '@mui/material'
 import clsx from 'clsx'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { ROUTE_LIST } from './routes'
+
 export interface HedarDesktopProps {}
 export function HeaderDesktop(props: HedarDesktopProps) {
+  const { profile, logout } = useAuth()
   const router = useRouter()
+  const isLoggedIn = !!profile?.username
+  const routeList = ROUTE_LIST.filter((route) => !route.requireLogin || isLoggedIn)
   return (
     <Box display={{ xs: 'none', md: 'block' }} py={2}>
       <Container>
         <Stack direction="row" justifyContent="flex-end">
-          {ROUTE_LIST.map((route) => (
+          {routeList.map((route) => (
             <Link href={route.path} key={route.label}>
               <MuiLink
                 sx={{ ml: 2, fontWeight: 'medium' }}
@@ -21,6 +25,16 @@ export function HeaderDesktop(props: HedarDesktopProps) {
               </MuiLink>
             </Link>
           ))}
+          {!isLoggedIn && (
+            <Link href="/login">
+              <MuiLink sx={{ ml: 2, fontWeight: 'medium' }}>Login</MuiLink>
+            </Link>
+          )}
+          {isLoggedIn && (
+            <MuiLink sx={{ ml: 2, fontWeight: 'medium', cursor: 'pointer' }} onClick={logout}>
+              Logout
+            </MuiLink>
+          )}
         </Stack>
       </Container>
     </Box>
--- components/common/header/routes.ts
+++ components/common/header/routes.ts
@@ -14,5 +14,6 @@ export const ROUTE_LIST = [
   {
     label: 'Works',
     path: '/works',
+    requireLogin: true,
   },
 ]
