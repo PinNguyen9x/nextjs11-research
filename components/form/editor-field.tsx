69ea3bb574259029502bf552fdc6aaac45861f91
Pinnguyen <nguyenthanhpin95@gamil.com>
2024-08-11 17:32:05 +0700

    07-11 EditorField - Cloudinary upload widget

--- components/form/editor-field.tsx
+++ components/form/editor-field.tsx
@@ -1,6 +1,6 @@
 import { Box, FormHelperText, Typography } from '@mui/material'
 import dynamic from 'next/dynamic'
-import { LegacyRef, useRef } from 'react'
+import { LegacyRef, useCallback, useEffect, useRef } from 'react'
 import { Control, FieldValues, Path, useController } from 'react-hook-form'
 import ReactQuill, { ReactQuillProps } from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
@@ -36,6 +36,11 @@ export function EditorField<T extends FieldValues>({ name, label, control }: Edi
     name,
     control,
   })
+  const imageHandler = useCallback(() => {
+    // @ts-ignore no type support
+    if (cloudinaryWidgetRef.current) cloudinaryWidgetRef.current.open?.()
+  }, [])
+
   const modules = {
     toolbar: {
       container: [
@@ -47,7 +52,7 @@ export function EditorField<T extends FieldValues>({ name, label, control }: Edi
         ['clean'],
       ],
       handlers: {
-        // image: imageHandler,
+        image: imageHandler,
       },
     },
   }
@@ -69,6 +74,52 @@ export function EditorField<T extends FieldValues>({ name, label, control }: Edi
   ]
 
   const editorRef = useRef(null)
+  const cloudinaryWidgetRef = useRef(null)
+  useEffect(() => {
+    function initCloudinaryWidget() {
+      // check and retry if cloudinary not ready
+      // @ts-ignore
+      if (!window.cloudinary) {
+        console.log('cloudinary not ready, trigger retry in 500ms')
+        setTimeout(initCloudinaryWidget, 500)
+        return
+      }
+
+      console.log('cloudinary is ready')
+      // @ts-ignore no type def support yet
+      const widget = window.cloudinary.createUploadWidget(
+        {
+          cloudName: 'nip',
+          uploadPreset: 'lern-nextjs',
+          multiple: false, //restrict upload to a single file
+          clientAllowedFormats: ['image'], //restrict uploading to image files only
+          maxImageFileSize: 2000000, //restrict file size to less than 2MB
+          // sources: [ "local", "url"], // restrict the upload sources to URL and local files
+          // folder: "user_images", //upload files to the specified folder
+          // tags: ["users", "profile"], //add the given tags to the uploaded files
+          // context: {alt: "user_uploaded"}, //add the given context data to the uploaded files
+          // maxImageWidth: 2000, //Scales the image down to a width of 2000 pixels before uploading
+          // theme: "purple", //change to a purple theme
+        },
+        // @ts-ignore no type support yet
+        (error, result) => {
+          if (!error && result && result.event === 'success') {
+            const quill = editorRef.current
+            // @ts-ignore
+            const range = quill?.getEditorSelection?.()
+            if (quill && range) {
+              // @ts-ignore
+              quill.getEditor()?.insertEmbed?.(range.index, 'image', result.info?.secure_url)
+            }
+          }
+        },
+      )
+
+      cloudinaryWidgetRef.current = widget
+    }
+
+    initCloudinaryWidget()
+  }, [])
 
   return (
     <Box>
